b733a3f8a26b9c0c20fe52c9c6d132ea314495df
Lucas <lucas.s.augusto@hotmail.com>
2025-01-28 08:50:29 -0300

    Function atualizarListaAmigos Added

--- app.js
+++ app.js
@@ -12,4 +12,16 @@ function adicionarAmigo(){
     }
 
     document.getElementById("amigo").value = "";
+    atualizarListaAmigos();
+}
+
+function atualizarListaAmigos(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    
+    for(let i = 0; i < amigos.length; i++){
+        let item = document.createElement("li");
+        item.textContent = amigos[i];
+        lista.appendChild(item);
+    }
 }
