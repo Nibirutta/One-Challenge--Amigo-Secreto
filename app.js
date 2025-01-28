6a5be9dd5a6b522b7f1b72328c6f0c9c70455d87
Lucas <lucas.s.augusto@hotmail.com>
2025-01-28 08:32:06 -0300

    Function AdicionarAmigos Added

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo(){
+    let nomeAmigo = document.getElementById("amigo").value;
+
+    if(amigos.includes(nomeAmigo)){
+        alert("Amigo já adicionado!");
+    } else if (nomeAmigo == ""){
+        alert("Digite o nome do amigo!");        
+    } else {
+        amigos.push(nomeAmigo);
+    }
+
+    document.getElementById("amigo").value = "";
+}
